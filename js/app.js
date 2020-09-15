6f61b0b9f516a9ca613bbcc09df39f5fdecd8bdb
Axel Segundo <axeljasegundo@gmail.com>
2020-09-15 18:49:21 -0500

    Validando formulario

--- js/app.js
+++ js/app.js
@@ -9,5 +9,36 @@ window.addEventListener('load', () => {
 function buscarClima(e) {
     e.preventDefault();
 
-    console.log('Buscando el clima');
+    // Validar
+    const ciudad = document.querySelector('#ciudad').value;
+    const pais = document.querySelector('#pais').value;
+
+    if(ciudad === '' || pais === '') {
+        mostrarError('Ambos campos son obligatorios');
+
+        return;
+    }
+}
+
+function mostrarError(mensaje) {
+    const alerta = document.querySelector('.bg-red-100');
+
+    if(!alerta) {
+        // Crear una alerta
+        const alerta = document.createElement('div');
+
+        alerta.classList.add('bg-red-100', 'border-red-400', 'text-red-700', 'px-4', 'py-3', 'rounded', 'max-w-nd', 'nx-auto', 'mt-6', 'text-center');
+
+        alerta.innerHTML = `
+            <strong class="font-bold">¡Error!</strong>
+            <span class="block">${mensaje}</span>
+        `;
+
+        container.appendChild(alerta);
+
+        // Se elimine la alerta
+        setTimeout(() => {
+            alerta.remove();
+        }, 5000);
+    }
 }
